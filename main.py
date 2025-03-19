f0fd267fd6a3601b33ea51f5f3695c6ec70d71f1
anzigar <ansigare@gmail.com>
2025-03-19 15:13:11 +0300

    Remove unused port retrieval from environment variables and use settings.PORT directly in uvicorn.run

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ app.include_router(router, prefix="/api/v1", tags=["questionnaire"])
 if __name__ == "__main__":
     import uvicorn
     
-    # Get port from environment variable or use default 8000
-    port = int(os.getenv("PORT", 8000))
+   
     
     uvicorn.run(app="main:app", host="0.0.0.0", port= settings.PORT)
